a0008853e424b2113983ad9e9ecb0f44dd4f167b
Dioni A. Oliveira <dionialves@gmail.com>
2026-05-20 20:20:34 -0300

    US-007-T1: Ajuste de configuracoes

--- src/main/java/dev/zayt/atrilha/AtrilhaApplication.java
+++ src/main/java/dev/zayt/atrilha/AtrilhaApplication.java
@@ -2,8 +2,10 @@ package dev.zayt.atrilha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("dev.zayt.atrilha")
 public class AtrilhaApplication {
     public static void main(String[] args) {
         SpringApplication.run(AtrilhaApplication.class, args);
